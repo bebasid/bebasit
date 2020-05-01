14c9ca6682e9589dc9e482258f047c6fc474c7ee
Palguno Wicaksono <palgunowicaksono@gmail.com>
2020-05-01 03:03:10 +0000

    Update and rename bebasit-mac.sh to bebasit.sh

--- shell/bebasit.sh
+++ shell/bebasit.sh
@@ -3,7 +3,6 @@
 about(){
   echo "Name of File  : bebasid tunnel"
   echo "Version       : 2020.5 (ALPHA)"
-  echo "Need Tester   : MacOS (OSX)"
   echo
   echo "Built with love by haibara and Dan008 for bebasid"
   echo "Especially thanks to:"
@@ -53,6 +52,24 @@ perintahin(){
   fi
 }
 tunnelin(){
+  getUname=$(uname -s)
+  case $getUname in
+    Linux* )
+      random=$(shuf -i 6000-8000 -n 1)
+      if [[ -x $(command -v google-chrome-stable) ]]; then
+        browser="google-chrome-stable"
+        killall chrome
+      elif [[ -x $(command -v google-chrome) ]]; then
+        browser="google-chrome"
+        killall chrome
+      fi
+      ;;
+    Darwin* )
+      random=$(jot -r 1 6000 8000)
+      browser="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
+      killall 'Google Chrome'
+      ;;
+  esac
   perintahin $1
   tmux has-session -t bebasid-tunnel 2>/dev/null
   if [ $? != 0 ]; then
@@ -61,7 +78,6 @@ tunnelin(){
     tmux kill-session -t bebasid-tunnel
     tmux new-session -d -s bebasid-tunnel -x 252 -y 29
   fi
-  random=$(jot -r 1 6000 8000)
   tmux split-window -v
   i=1
   while [[ "$bisa"=="no" ]]; do
@@ -72,15 +88,17 @@ tunnelin(){
       tmux kill-session -t bebasid-tunnel
       exit 1
     fi
-    dns=$(curl https://two-ply-mixtures.000webhostapp.com/pt.php?id=$i --silent)
-    loadin 0.01 "[$i] Mendapatkan DNS $dns"
     if [[ "$1" == "Green Tunnel" ]]; then
+      dns=$(curl https://two-ply-mixtures.000webhostapp.com/gt.php?id=$i --silent)
+      loadin 0.01 "[$i] Mendapatkan DNS $dns"
       echo "Tunnel: Green Tunnel"
       tmux send-keys -t 1 "gt --ip 127.0.0.1 --port $random --dns-server $dns --system-proxy false --silent true -v 'green-tunnel:*'" Enter
     elif [[ "$1" == "PowerTunnel" ]]; then
+      dns=$(curl https://two-ply-mixtures.000webhostapp.com/pt.php?id=$i --silent)
+      loadin 0.01 "[$i] Mendapatkan DNS $dns"
       echo "Tunnel: PowerTunnel"
       db="https://raw.githubusercontent.com/bebasid/bebasid/master/dev/scripts/goodbyedpi/blacklist.txt"
-      tmux send-keys -t 1 "java -jar ~/.bebasit/PowerTunnel.jar -start -console -government-blacklist-from $db -use-doh-resolver $dns -ip 127.0.0.1 -p $random -debug -disable-auto-proxy-setup" Enter
+      tmux send-keys -t 1 "java -jar ~/.bebasit/PowerTunnel.jar -start -console -government-blacklist-from $db -use-doh-resolver $dns -ip 127.0.0.1 -port $random -debug -disable-auto-proxy-setup" Enter
     fi
     loadin 0.01 "Mengetes Koneksi $1 ke Netflix"
     sleep 5
@@ -95,10 +113,9 @@ tunnelin(){
       ((i++))
     fi
   done
-  browser="/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome"
-  killall 'Google Chrome'
+  cakepin 0.01 "Tunggu sebentar, sedang membuka $browser"
   tmux split-window -h
-  tmux send-keys -t 2 "$browser netflix.com --proxy-server=127.0.0.1:$random" Enter
+  tmux send-keys -t 2 "$browser https://www.netflix.com --proxy-server=127.0.0.1:$random" Enter
   tmux send-keys -t 0 "bebasit success $random $1" Enter
   tmux select-pane -t 0
   tmux a
@@ -115,7 +132,7 @@ case $1 in
     esac
     ;;
   install )
-    case $1 in
+    case $2 in
       gt )
         installin "green-tunnel"
         ;;
@@ -130,11 +147,11 @@ case $1 in
     echo "bebasit [command]"
     echo 
     echo "List command:"
-    echo "install   : Menginstall bahan-bahan yang diperlukan (HOMEBREW NPM, GT, TMUX)"
+    echo "install   : Menginstall bahan-bahan yang diperlukan"
     echo "--help    : Menampilkan bantuan"
-    echo "start     : Memulai sesi Green Tunnel + Google Chrome"
+    echo "start     : Memulai sesi bebasit + Google Chrome"
     echo "stop      : Mematikan sesi"
-    echo "update    : Memperbarui script bash BEBASID + Green Tunnel"
+    echo "update    : Memperbarui script bash bebasit"
     echo 
     echo "Aplikasi ini masih dalam tahap ujicoba alpha, sehingga mungkin banyak bug"
     ;;
@@ -142,7 +159,7 @@ case $1 in
     tmux kill-session -t bebasid-tunnel
     ;;
   update )
-    if sudo curl -o /usr/local/bin/bebasit https://raw.githubusercontent.com/bebasid/bebasit/master/shell/bebasit-mac.sh --progress-bar; then
+    if sudo curl -o /usr/local/bin/bebasit https://raw.githubusercontent.com/bebasid/bebasit/master/shell/bebasit.sh --progress-bar; then
       echo ""
       echo "Berhasil mengunduh"
       echo "Mengecek aplikasi"
