57b0c7256092423abb8e47a154923cd6bae587ab
gvoze32 <gvoze32@gmail.com>
2020-05-02 20:25:38 +0700

    add goodbyedpi

---
+++ dependencies/goodbyedpi/service_install_dnsredir_blacklistAdGuard.cmd
@@ -0,0 +1,17 @@
+@ECHO OFF
+PUSHD "%~dp0"
+set _arch=x86
+IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" (set _arch=x86_64)
+IF DEFINED PROCESSOR_ARCHITEW6432 (set _arch=x86_64)
+
+echo This script should be run with administrator privileges.
+echo Right click - run as administrator.
+echo Press any key if you're running it as administrator.
+pause
+sc stop "GoodbyeDPI"
+sc delete "GoodbyeDPI"
+sc create "GoodbyeDPI" binPath= "\"%CD%\%_arch%\goodbyedpi.exe\" -1 --dns-addr 176.103.130.130 --dnsv6-addr 2a00:5a60::ad1:0ff --blacklist \"%CD%\blacklist.txt\"" start= "auto"
+sc description "GoodbyeDPI" "Passive Deep Packet Inspection blocker and Active DPI circumvention utility"
+sc start "GoodbyeDPI"
+
+POPD
